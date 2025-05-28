3d5163f9229092f3c30f43ef5b7a255113c7f508
catzz <2040058407@qq.com>
2025-05-28 14:00:37 +0800

    message 6: Functional test now checks we can input a to-do item

--- functional_tests.py
+++ functional_tests.py
@@ -1,5 +1,8 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 import unittest
+from selenium.webdriver.common.by import By
 
 class NewVisitorTest(unittest.TestCase):
 
@@ -11,9 +14,33 @@ class NewVisitorTest(unittest.TestCase):
 
     def test_can_start_a_list_and_retrieve_it_later(self):
 
+        #首页
         self.browser.get('http://localhost:8000')
+        
+        #标题和头部都有“To-Do”
+        self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element(By.TAG_NAME, 'h1').text
+        self.assertIn('To-Do', header_text)
 
-        self.assertIn('To-Do', self.browser.title), "browser title was:" + self.browser.title
+        #文本输入框
+        inputbox = self.browser.find_element(By.ID, 'id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-da item'
+        )
+
+        #输入框中输入“Buy flowers”
+        inputbox.send_keys('Buy flowres')
+
+        #按回车键更新页面
+        #待办事项显示“1: Buy flowers”
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_element(By.ID, 'id_list_table')
+        rows = table.find_elements(By.TAG_NAME, 'tr')
+        self.assertIn('1: Buy flowers', [row.text for row in rows])
+        
         self.fail('Finish the test!')
 
 if __name__ == '__main__':
